92ea2dfd62474bbd5ed80630ef211ae5709cafde
Your Name <your_email@example.com>
2026-02-24 22:21:04 +0530

    usermodel

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// require("dotenv").config();
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: ".env" });
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running on PORT ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed",err);
+})
---
+++ utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (func) => {
+    return (req,res,next) => {
+        Promise.resolve(func(req,res,next)).catch((err) => next(err))
+    }
+}
+
+
+export default {asyncHandler}
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try{
+//         await fn(req,res,next)
+//     }
+//     catch(err){
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
